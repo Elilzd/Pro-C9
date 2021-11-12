c26de3449f29c8f9e098a84fcfb918fa83d9b888
Elilzd <94006517+Elilzd@users.noreply.github.com>
2021-11-12 11:38:31 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,34 @@
-var box;
-
 function setup() {
-   createCanvas(400,400);
-   box = createSprite(200,200,40,40);
+  createCanvas(1200,600);
+  background(51);
+  box = createSprite(550,250,30,30);
 }
-
 
 function draw() {
-    background(blue);
-    drawSprites();
+
+  // escribir el código para cambiar el color del fondo, rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha) 
+  if (keyIsDown(RIGHT_ARROW)){
+    background("red");
+  }
+  
+
+  if (keyIsDown(LEFT_ARROW)) {
+    background("blue");
+    box.positionX
+    
+  }
+
+ 
+    if (keyIsDown(UP_ARROW)) {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    background("green");
+  }
+
+  
+  drawSprites();
 }
-
-
-
 
